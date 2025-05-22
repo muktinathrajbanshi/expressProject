49ab19548f3810bef2a5706bf2f6f67b3e731ae4
unknown <muktinathrajbanshi786@gmail.com>
2025-05-22 23:26:12 +0545

    my new project started

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    res.send("Hello how are you!");
+})
+
+
+
+// server create
+app.listen(port, () => {
+    console.log(`server is running at port no ${port}`);
+})
